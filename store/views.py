f07ccfc681cf9a2a5472af6ad3f32088b128b1c0
swagasoft <swagasoft@gmail.com>
2022-10-08 12:44:51 +0100

    added hyperlink

--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
 from http.client import responses
+from itertools import product
 from multiprocessing import context
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse, HttpRequest
@@ -9,28 +10,42 @@ from .serializer import ProductSerializer
 from rest_framework import status
 # Create your views here.
 
-@api_view()
+@api_view(['GET','POST'])
 def product_list(request):
-    queryset = Product.objects.select_related('collection').all()
-    serialize = ProductSerializer(queryset, many=True, context ={'request': request})
-    return Response(serialize.data)
+    if request.method == 'GET':
+
+        queryset = Product.objects.select_related('collection').all()
+        serialize = ProductSerializer(queryset, many=True, context ={'request': request})
+        return Response(serialize.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+       
+
+        
 
 
 
 
-@api_view()
+
+@api_view(['GET','PUT','DELETE'])
 def product_detail(request, id: int):
-    # try:
-
-    #     product = Product.objects.get(pk=id)
-    #     serialize = ProductSerializer(product)
-    #     return Response(serialize.data)
-    # except Product.DoesNotExist:
-    #     return Response(status=status.HTTP_404_NOT_FOUND)
-
     product = get_object_or_404(Product, pk=id)
-    serialize = ProductSerializer(product)
-    return Response(serialize.data)
+    if request.method == 'GET':
+        serialize = ProductSerializer(product)
+        return Response(serialize.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product,data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 @api_view()
 def collection_details(request, pk:int):
